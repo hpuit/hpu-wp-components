3253181af89f25494ceca898cbb2657f2090096c
Ian Watkins <iwatkins@highpoint.edu>
2024-11-28 01:32:31 -0500

    package.json and webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'production',
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'index.js',
+		library: '@hpu-wp/components',
+		libraryTarget: 'commonjs2',
+		globalObject: 'this'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env", "@babel/preset-react"]
+					}
+				}
+			}
+		]
+	},
+	externals: {
+		react: 'React',
+		'react-dom': 'ReactDOM',
+		'@wordpress/components': ['wp', 'components'],
+		'@wordpress/block-editor': ['wp', 'blockEditor'],
+		'@wordpress/blocks': ['wp', 'blocks'],
+		'@wordpress/element': ['wp', 'element'],
+		'@wordpress/data': ['wp', 'data'],
+		'@wordpress/i18n': ['wp', 'i18n']
+	},
+	resolve: {
+		extensions: ['.js', '.jsx']
+	}
+};

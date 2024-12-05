e71d9ea041712776f0205c27467a34d41b402c07
Ian Watkins <iwatkins@highpoint.edu>
2024-12-05 13:10:56 -0500

    clean build for 0.3.0

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-element'), 'version' => 'accfd976773f92328d1f');
+<?php return array('dependencies' => array('wp-components', 'wp-element'), 'version' => 'e19d84e732ce5a29ae9f');

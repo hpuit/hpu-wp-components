22dcdb17f6846ca7201b4587a791fda7b85e3424
iwatkinshpu <111886523+iwatkinshpu@users.noreply.github.com>
2024-12-12 09:34:00 -0500

    Feature/select blog controls (#2)
    * renamed BlogSelectControl to SelectBlogControl, initial draft
    
    * update index.js to reflect SelectBlogControls
    
    * update PostSearchControls to prevent search loop
    
    * debugging not updating on blogID/postType updates
    
    * working selectblog controls and fixes to state/prop rendering
    
    * clean build
    
    * version increment to 0.4,0
    
    * v 0.4.1 - conditionally use HPUCustomBlocksData object to lookup blogPath quicker

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-element'), 'version' => '95461d2305dc5523c255');
+<?php return array('dependencies' => array('wp-components', 'wp-element'), 'version' => 'c2806751e5373f8f01e8');

0249eb9216e5fc8dee03b347c75a0b3786555c58
Ian Watkins <iwatkins@highpoint.edu>
2024-12-16 08:43:58 -0500

    add control to script, update version to 0.5.0

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-element'), 'version' => 'c2806751e5373f8f01e8');
+<?php return array('dependencies' => array('wp-components', 'wp-element'), 'version' => 'ab6dd83ce3a78ee8eec9');

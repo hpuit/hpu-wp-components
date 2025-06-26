0e798d9d9b93aa04a71cd70eada3271fac6c72a4
Ian Watkins <iwatkins@highpoint.edu>
2025-06-26 09:09:08 -0400

    build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-element'), 'version' => 'f1192f2ffdbc4db61a4d');
+<?php return array('dependencies' => array('wp-components', 'wp-element'), 'version' => '74b63fd1edd3f114406c');
